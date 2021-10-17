cb62b91cb030fd3d1014eb8da0e52ad2dd61b4ef
Darius Duta <dduta065@gmail.com>
2021-10-18 00:42:04 +0100

    use helmet

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
-import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.use(helmet());
 
   await app.listen(3000);
 }
+
 bootstrap();
