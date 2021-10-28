f55f992bf2643aa61ecd7d708d7520f107c7a252
Darius Duta <dduta065@gmail.com>
2021-10-28 15:16:01 +0100

    configuration tests

---
+++ src/config/configuration.spec.ts
@@ -0,0 +1,56 @@
+import * as fs from 'fs';
+import * as jsyaml from 'js-yaml';
+import { join } from 'path';
+
+import * as configuration from './configuration';
+
+describe('configuration', () => {
+  it.each`
+    envType
+    ${'test'}
+    ${'dev'}
+    ${'prod'}
+  `(
+    `should be defined for $envType env`,
+    ({ envType }: { envType: string }) => {
+      process.env.NODE_ENV = envType;
+
+      const fileName = join(__dirname, `config.${envType}.yaml`);
+      const readFileSyncMock = jest.fn(() => `${envType} config`);
+
+      const loadMock = jest.fn((configString: string) => `${configString} obj`);
+
+      jest.spyOn(fs, 'readFileSync').mockImplementation(readFileSyncMock);
+      jest.spyOn(jsyaml, 'load').mockImplementation(loadMock);
+
+      const res = configuration.default();
+
+      expect(readFileSyncMock).toBeCalledWith(fileName, 'utf8');
+      expect(loadMock).toBeCalledWith(`${envType} config`);
+
+      expect(res).toStrictEqual(`${envType} config obj`);
+    },
+  );
+
+  it(`should throw an error if NODE_ENV is not defined`, () => {
+    process.env.NODE_ENV = '';
+
+    try {
+      configuration.default();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as Error).message).toBe('NODE_ENV is not defined');
+    }
+  });
+
+  it(`should throw an error if NODE_ENV is not ['test', 'dev', 'prod']`, () => {
+    process.env.NODE_ENV = 'another';
+
+    try {
+      configuration.default();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect((error as Error).message).toBe('NODE_ENV value is not accepted');
+    }
+  });
+});
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -2,9 +2,17 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
-const YAML_CONFIG_FILENAME = `config.${process.env.NODE_ENV ?? 'dev'}.yaml`;
-
 export default () => {
+  const YAML_CONFIG_FILENAME = `config.${process.env.NODE_ENV}.yaml`;
+
+  if (!process.env.NODE_ENV) {
+    throw new Error('NODE_ENV is not defined');
+  }
+
+  if (!['test', 'dev', 'prod'].includes(process.env.NODE_ENV)) {
+    throw new Error('NODE_ENV value is not accepted');
+  }
+
   const parsedConfig = yaml.load(
     readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
   ) as Record<string, any>;
