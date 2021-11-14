c8b0b4063224b379148d5459925f65ede638c5d2
Darius Duta <dduta065@gmail.com>
2021-11-14 23:42:51 +0000

    restrict group resolver to admins only

--- src/group/group.resolver.ts
+++ src/group/group.resolver.ts
@@ -1,10 +1,17 @@
+import { UseGuards } from '@nestjs/common';
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+
+import { Role } from '../auth/enums/role.enum';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
 import { Group } from './group.entity';
 import { GroupService } from './group.service';
 import { CreateGroupInput } from './dto/create-group.input';
 import { UpdateGroupInput } from './dto/update-group.input';
 
+@UseGuards(RolesGuard)
+@Roles(Role.User, Role.Admin)
 @Resolver(() => Group)
 export class GroupResolver {
   constructor(private groupService: GroupService) {}
